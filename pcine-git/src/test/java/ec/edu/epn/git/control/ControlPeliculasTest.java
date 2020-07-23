98ecfe52c5467143237ad667ecb602eb2d7d3cee
Ramses <ramses.paredes@epn.edu.ec>
2020-07-23 18:09:33 -0500

    Pruebas funcionales y ControlPeliculas Update

--- pcine-git/src/test/java/ec/edu/epn/git/control/ControlPeliculasTest.java
+++ pcine-git/src/test/java/ec/edu/epn/git/control/ControlPeliculasTest.java
@@ -2,38 +2,51 @@ package ec.edu.epn.git.control;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class ControlPeliculasTest {
+import ec.edu.epn.git.dto.PeliculaDTO;
 
-	@Test
-	public void testControlPeliculas() {
-		fail("Not yet implemented");
+public class ControlPeliculasTest {
+	private ControlPeliculas control;
+	
+	@Before
+	public void before() {
+		this.control = new ControlPeliculas();
+		control.addPelicula(new PeliculaDTO(0, "nombre", "genero", 2020, 200));
+		control.addPelicula(new PeliculaDTO(1, "nombre", "genero", 2020, 200));
 	}
+	
 
 	@Test
 	public void testAddPelicula() {
-		fail("Not yet implemented");
+		assertTrue(control.addPelicula(new PeliculaDTO(2, "nombre", "genero", 2020, 200)));
 	}
 
 	@Test
 	public void testDeletePelicula() {
-		fail("Not yet implemented");
+		assertTrue(control.deletePelicula(0));
 	}
+	
 
 	@Test
 	public void testFindAllPeliculas() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		assertEquals(2, control.findAllPeliculas().size());
 	}
 
 	@Test
 	public void testUpdatePelicula() {
-		fail("Not yet implemented");
+		PeliculaDTO expected= new PeliculaDTO(0, "nombreasdf", "genero", 2020, 200);
+		//assertEquals(expected.getIdPelicula() ,control.findPeliculaById(0).getIdPelicula());
+		assertTrue(control.updatePelicula(0, expected));
 	}
 
 	@Test
 	public void testFindPeliculaById() {
-		fail("Not yet implemented");
+		PeliculaDTO expected= new PeliculaDTO(0, "nombre", "genero", 2020, 200);
+		assertEquals(expected.getIdPelicula() ,control.findPeliculaById(0).getIdPelicula());
+		
 	}
 
 }
